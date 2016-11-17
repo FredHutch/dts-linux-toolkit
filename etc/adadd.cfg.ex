8669d228699138d44d2db667ab0bbb686a4471c1
Michael Gutteridge <mrg@fhcrc.org>
2016-11-17 11:51:36 -0800

    some updates for example config files

--- etc/adadd.cfg.ex
+++ etc/adadd.cfg.ex
@@ -1,1 +1,5 @@
+#
+# This needs to point to the msktjoin tool
 msktjoin=/path/to/msktjoin
+
+# vim: set filetype=bash:
--- etc/msktjoin.cfg.ex
+++ etc/msktjoin.cfg.ex
@@ -1,3 +1,9 @@
-ou='ou=Workstations,ou=Computers,ou=SciComp'
+# Use bash syntax for this file
+
+# AD Domain
 domain='fhcrc.org'
 
+# Create machine accounts in this OU:
+ou='ou=Workstations,ou=Computers,ou=Group'
+
+# vim: set filetype=bash:
